095e6128e9ca057b2e11db6ba6a7ffc6626845c5
Steve Davis <deevodavis71@gmail.com>
2017-11-28 19:27:24 +0000

    Playing wth classloaders

---
+++ src/main/java/com/example/demo/profiles/DefaultValues.java
@@ -0,0 +1,17 @@
+package com.example.demo.profiles;
+
+/**
+ * User: stevedavis
+ * Date: 28/11/2017
+ * Time: 16:53
+ * Description:
+ */
+public class DefaultValues {
+
+    public final static String NAME = "Steve";
+
+    public static String doIt() {
+        return "42";
+    }
+
+}
--- src/main/java/com/example/demo/web/TestController.java
+++ src/main/java/com/example/demo/web/TestController.java
@@ -1,7 +1,12 @@
 package com.example.demo.web;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.profiles.DefaultValues;
 
 /**
  * User: stevedavis
@@ -14,10 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping("/test")
-    public String test() {
+    public String test() throws Exception {
+
+        //Get the System Classloader
+        ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+
+        //Get the URLs
+        ClassLoader effectiveClassLoader = getClass().getClassLoader();
+        URL[] classPath = ((URLClassLoader) effectiveClassLoader).getURLs();
+        for (URL u : classPath) {
+            System.out.println(u);
+        }
 
         System.out.println(new java.util.Date() + " ... got a call");
-        return "Hello World!";
+
+        Class<?> dv = Class.forName("com.example.demo.profiles.DefaultValues");
+        String name = (String) dv.getDeclaredField("NAME").get(null);
+
+        System.out.println("Static loaded: " + DefaultValues.NAME);
+        System.out.println("Static loaded: " + DefaultValues.doIt());
+
+        return "Hello World from " + name + "!";
 
     }
 
