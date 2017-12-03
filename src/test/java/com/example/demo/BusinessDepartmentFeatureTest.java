aa0be4ef9964d4f490ef9b439ea4c610721bb109
Steve Davis <deevodavis71@gmail.com>
2017-12-03 16:04:32 +0000

    Added Cucumber options

--- src/test/java/com/example/demo/BusinessDepartmentFeatureTest.java
+++ src/test/java/com/example/demo/BusinessDepartmentFeatureTest.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, features = "classpath:features")
 public class BusinessDepartmentFeatureTest {
 
 }
