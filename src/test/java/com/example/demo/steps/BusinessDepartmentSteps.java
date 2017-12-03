f7d5008fb015fd1102cb771bed2d27dabfbbaca6
Steve Davis <deevodavis71@gmail.com>
2017-12-03 12:34:41 +0000

    Rename files

--- src/test/java/com/example/demo/steps/BusinessDepartmentSteps.java
+++ src/test/java/com/example/demo/steps/BusinessDepartmentSteps.java
@@ -20,7 +20,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class BusinessObjectSteps {
+public class BusinessDepartmentSteps {
 
     @Mock
     private HumanResourceSystem hrSystem;
